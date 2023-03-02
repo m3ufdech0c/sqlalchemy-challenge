b530bffcb0a8c2be2d8938216d3523cea04cb7bc
m3ufdech0c <aidadaz@gmail.com>
2023-03-02 16:01:44 -0600

    Edited comments on both app.py and climate.ipynb files

--- SurfUp/app.py
+++ SurfUp/app.py
@@ -1,4 +1,4 @@
-# Import Flask and other required dependencies 
+# Importing Flask and other required dependencies 
 from flask import Flask, jsonify
 
 import sqlalchemy
@@ -15,15 +15,16 @@ import datedelta as dd
 # Database Setup
 #################################################
 
-engine = create_engine("sqlite:////Resources/hawaii.sqlite")
+# Creating an engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
-# reflect an existing database into a new model
+# Reflecting an existing database into a new model
 Base = automap_base()
 
-# reflect the tables
+# Reflecting the tables
 Base.prepare(autoload_with=engine)
 
-# Save references to each table
+# Saving references to each table
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
@@ -31,7 +32,6 @@ Station = Base.classes.station
 # Flask Setup
 #################################################
 
-# Create my climate app
 app = Flask(__name__)
 
 #################################################
@@ -53,29 +53,29 @@ def welcome():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-     # Create our session (link) from Python to the DB
+     # Creating our session (link) from Python to the DB
     session = Session(engine)
 
     """Return the precipitation analysis data as json"""
 
-    # Calculate the date one year from the last date in data set.
+    # Calculating the date one year from the last date in data set.
     query_date_limit = dt.date(2017, 8, 23) - 12 * dd.MONTH
 
-    # Perform a query to retrieve the data and precipitation scores
+    # Performing a query to retrieve the data and precipitation scores
     one_yr_data = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date <= '2017-08-23').\
     filter(Measurement.date >= query_date_limit).order_by(Measurement.date.desc()).all()
 
     # Closing Session
     session.close()
 
-    # Convert list of tuples into normal list
+    # Converting list of tuples into normal list
     prcp_by_date = list(np.ravel(one_yr_data))
 
     return jsonify(prcp_by_date)
 
 @app.route("/api/v1.0/stations")
 def stations():
-    # Create our session (link) from Python to the DB
+    # Creating our session (link) from Python to the DB
     session = Session(engine)
 
     """Return a json list of stations from the dataset"""
@@ -85,18 +85,18 @@ def stations():
     # Closing Session
     session.close()
 
-    # Convert list of tuples into normal list
+    # Converting list of tuples into normal list
     station_list = list(np.ravel(stations))
 
     return jsonify(station_list)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    # Create our session (link) from Python to the DB
+    # Creating our session (link) from Python to the DB
     session = Session(engine)
     """Return a json list of temperature observations for the previous year""" 
     
-    # Calculate the date one year from the last date in data set.
+    # Calculating the date one year from the last date in data set.
     query_date_limit = dt.date(2017, 8, 23) - 12 * dd.MONTH
 
     # Performing a query to list the dates and temperature observations of the most-active station for the previous year of data
@@ -113,14 +113,14 @@ def tobs():
     # Closing Session
     session.close()
 
-    # Convert list of tuples into normal list
+    # Converting list of tuples into normal list
     last_yr_act_station_tobs_list = list(np.ravel(last_yr_act_station_tobs))
 
     return jsonify(last_yr_act_station_tobs_list)
 
 @app.route("/api/v1.0/<start>")
 def measurement_date(start):
-    # Create our session (link) from Python to the DB
+    # Creating our session (link) from Python to the DB
     session = Session(engine)
     """Return a json list of the minimum temperature, the average temperature, and the maximum temperature for a specified start"""
     
@@ -134,7 +134,7 @@ def measurement_date(start):
     
     # Converting the tuple from the above into a dictionary for dates greater or equal to start date
     tobs_up_to_start_date_dict = dict()
-    for Measurement.date in tobs_up_to_start_date:
+    for Measurement.date, tmin, tavg, tmax in tobs_up_to_start_date:
         tobs_up_to_start_date_dict.setdefault(Measurement.date, []).append(tmin)
         tobs_up_to_start_date_dict.setdefault(Measurement.date, []).append(tavg)
         tobs_up_to_start_date_dict.setdefault(Measurement.date, []).append(tmax)
